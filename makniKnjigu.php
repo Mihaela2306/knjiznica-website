829d32d13da34ab0cadd73bee6bb84921420bda7
Mihaela Čalogović <46655278+kraljtomislav@users.noreply.github.com>
2021-12-12 22:18:33 +0100

    Update makniKnjigu.php

--- makniKnjigu.php
+++ makniKnjigu.php
@@ -6,15 +6,15 @@ if(isset($_GET["knjigaId"])){
 	$idKnjige = $_GET["knjigaId"];
 	$sql = "SELECT * FROM knjige WHERE id='" . $idKnjige ."';";
 }else{
-	header("Location: ../knjiznica/knjiga.php?knjigaId=" . $idKnjige . "&promjena=error");
+	header("Location: ../knjiga.php?knjigaId=" . $idKnjige . "&promjena=error");
 }
 $idKorisnika = $_SESSION["id"];
 
 $query = "DELETE FROM korisnik_knjiga WHERE korisnikId='" . $idKorisnika ."' AND knjigaId = '".$idKnjige . "'";
 $result = mysqli_query($conn, $query);
 	if($result){
-		header("Location: ../knjiznica/knjiga.php?promjena=success&knjigaId=" . $idKnjige . "");
+		header("Location: ../knjiga.php?promjena=success&knjigaId=" . $idKnjige . "");
 	} else {
-		header("Location: ../knjiznica/knjiga.php?KnjigaId=" . $idKnjige . "&promjena=error");
+		header("Location: ../knjiga.php?KnjigaId=" . $idKnjige . "&promjena=error");
 	}
 
