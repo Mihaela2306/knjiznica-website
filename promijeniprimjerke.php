9a53c2290a400cbfc09d5038dcd09b11877677cb
Mihaela Čalogović <46655278+kraljtomislav@users.noreply.github.com>
2021-12-12 22:25:24 +0100

    Update promijeniprimjerke.php

--- promijeniprimjerke.php
+++ promijeniprimjerke.php
@@ -6,10 +6,10 @@ if(isset($_GET["knjigaId"]) && isset($_GET["broj"])){
 	$idKnjige = $_GET["knjigaId"];
 	$broj = $_GET["broj"];
 } else {
-	header("Location: ../knjiznica/knjiga.php?knjigaId=" . $idKnjige . "&promjenabr=error");
+	header("Location: ../knjiga.php?knjigaId=" . $idKnjige . "&promjenabr=error");
 }
 
 $query = "UPDATE knjige SET brPrimjeraka = " . $broj . " WHERE id = '" . $idKnjige . "'";
 $result = mysqli_query($conn, $query);
 
-header("Location: ../knjiznica/knjiga.php?promjenabr=success&knjigaId=" . $idKnjige);
+header("Location: ../knjiga.php?promjenabr=success&knjigaId=" . $idKnjige);
